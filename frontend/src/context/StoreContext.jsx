2eadd8dfa68602b8610a2bd9ecdb1ad8394748e9
Bolli Hari <bollihari18@gmail.com>
2025-07-09 20:25:40 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -8,7 +8,7 @@ const StoreContextProvider = (props) => {
 
 
     const [cartItems,setcartItems] = useState({})
-    const url = "http://localhost:4000"
+    const url = "https://food-order-app-lkuf.onrender.com"
     const [token,setToken] = useState("")
     const [food_list,setFood_list] = useState([])
 
